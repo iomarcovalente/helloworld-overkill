c67a1ed567187b55d6db9c818c0453d9e1df4618
Marco Valente <marco.valente@sky.uk>
2021-11-13 20:21:53 +0000

    Updated build and push to dynamically parse vars

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,16 +2,25 @@
 
 set -ex
 
+if [[ -z ${1} ]];then
+    IMAGE_NAME=`gh repo view --json name | jq -r .name`
+else
+    IMAGE_NAME=${1}
+fi
+
+if [[ -z ${2} ]];then
+    TAG=$(git rev-parse --short HEAD)
+else
+    TAG=${2}
+fi
+
 if [[ -z ${3} ]];then
     REGISTRY="localhost:5000"
 else
     REGISTRY=${3}
 fi
 
-set -u
-
-IMAGE_NAME="${1}"
-TAG="${2}"
-
 docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} -t ${REGISTRY}/${IMAGE_NAME}:latest .
-docker push ${REGISTRY}/${IMAGE_NAME}
+
+docker push ${REGISTRY}/${IMAGE_NAME}:${TAG}
+docker push ${REGISTRY}/${IMAGE_NAME}:latest
